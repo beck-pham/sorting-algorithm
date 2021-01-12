553570dd441a93d94c495e7dc7e79b4d26eb429f
beck_pham <chauvupham@gmail.com>
2021-01-12 12:46:28 -0800

    implemented sorting for other types of data

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 4, -5, 7]);
-const sorter = new Sorter(numbersCollection);
+
+// const numbersCollection = new NumbersCollection([10, 4, -5, 7]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+// const characterscollection = new CharactersCollection('XaeIJQdnj');
+// const sorter = new Sorter(characterscollection);
+// sorter.sort();
+// console.log(characterscollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(17);
+linkedList.add(-2);
+linkedList.add(9);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(numbersCollection.data);
-
+linkedList.print();
